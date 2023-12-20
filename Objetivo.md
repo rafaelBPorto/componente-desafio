c0b3ce8a43af252a948d799868707eebcc005787
rafaelporto <rafael.porto@techsavvy.com.br>
2023-12-20 08:20:29 -0300

    feat: components

---
+++ Objetivo.md
@@ -0,0 +1,3 @@
+# Objetivo
+
+Organizar o componente App em subcomponenentes, de forma que o componente App fique o mais simples possível.
--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,38 @@
 <template>
   <v-app>
+
+    <!-- Menu Esquerda -->
     <v-navigation-drawer app>
-      <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-                Menu
-            </v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-
+      <Toolbar></Toolbar>
       <v-divider></v-divider>
-
-      <v-list dense class="pt-0">
-        <v-list-item v-for="item in itensMenu" :key="item.titulo">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.titulo }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <MenuToolbar></MenuToolbar>
     </v-navigation-drawer>
 
-    <v-app-bar app>
-        <v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-        </v-toolbar-title>
-    </v-app-bar>
+    <!-- Conteudo -->
+    <AppBar></AppBar>
+    <Carroseul></Carroseul>
 
-    <v-content>
-      <v-carousel>
-        <v-carousel-item
-            v-for="(item,i) in fotos"
-            :key="i"
-            :src="item.src"></v-carousel-item>
-      </v-carousel>
-    </v-content>
-
-    <v-footer class="pa-3" app>
-      <v-spacer></v-spacer>
-      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+    <!-- Rodapé -->
+    <Footer></Footer>
+    
   </v-app>
 </template>
 
 <script>
+import AppBar from './AppBar.vue'
+import Carroseul from './Carroseul.vue'
+import Footer from './Footer.vue'
+import MenuToolbar from './MenuToolbar.vue'
+import Toolbar from './Toolbar.vue'
+
 export default {
-  data() {
-    return {
-      itensMenu: [
-        {titulo: 'Início', icon: 'mdi-view-dashboard'},
-        {titulo: 'Sobre', icon: 'mdi-forum'}
-      ],
-      fotos: [
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-        {src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-      ]
-    }
+  components:{
+    AppBar,
+    Carroseul,
+    Footer,
+    MenuToolbar,
+    Toolbar,
+
   }
 }
 </script>
