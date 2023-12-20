cc994a9f024f56773fbe83c7580dec2b8caf97a2
rafaelporto <rafael.porto@techsavvy.com.br>
2023-12-20 08:47:23 -0300

    chore

--- src/main/App.vue
+++ src/main/App.vue
@@ -19,11 +19,11 @@
 </template>
 
 <script>
-import AppBar from './AppBar.vue'
-import Carroseul from './Carroseul.vue'
-import Footer from './Footer.vue'
-import MenuToolbar from './MenuToolbar.vue'
-import Toolbar from './Toolbar.vue'
+import AppBar from '@/components/template/AppBar.vue'
+import Carroseul from '@/components/widgets/Carroseul.vue'
+import Footer from '@/components/template/Footer.vue'
+import MenuToolbar from '@/components/template/MenuToolbar.vue'
+import Toolbar from '@/components/template/Toolbar.vue'
 
 export default {
   components:{
